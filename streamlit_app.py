9a33d12754db03b23304193fdeb05b5dd41c90a6
Naveen-Kariyawasam <nvnkrwsm465@gmail.com>
2024-12-17 13:46:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,26 @@ with open(path_to_model, 'rb') as file:
     model = joblib.load(file)
 
 feature_names = joblib.load('./feature_names.pkl')
+ 
+#Load the label encoders for categorical columns
+label_encoders = {
+    'Gender': joblib.load('Gender_label_encoder.pkl'),
+    'Occupation': joblib.load('Occupation_label_encoder.pkl'),
+    'BMI Category': joblib.load('BMI_Category_label_encoder.pkl'),
+    'Blood Pressure': joblib.load('Blood_Pressure_label_encoder.pkl'),
+    'Sleep Disorder': joblib.load('Sleep_Disorder_label_encoder.pkl')
+}
+
+# List of features (should match the model training order)
+feature_names = [
+    'Age', 'Gender', 'Occupation', 'Hours of Sleep', 'Physical Activity Level',
+    'BMI Category', 'Blood Pressure', 'Sleep Disorder'
+]
+
+# Function to load animations for UI
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
