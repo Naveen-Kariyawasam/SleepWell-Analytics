924b1e2b876d26bc0c81a4150fe2025036f8e305
Naveen-Kariyawasam <nvnkrwsm465@gmail.com>
2024-12-17 13:36:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,11 @@ import pandas as pd
 import requests
 from sklearn.preprocessing import LabelEncoder
 from streamlit_lottie import st_lottie
+
+path_to_model = 'best_random_forest_model.joblib'
+
+with open(path_to_model, 'rb') as file:
+    model = joblib.load(file)
+
+feature_names = joblib.load('./feature_names.pkl')
+
